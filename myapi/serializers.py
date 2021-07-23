0770e357d7bc51c582f8f42fc78e80a8f2d049e1
Roman Tetelbaum <rtetelbaum@mailbox.org>
2021-07-22 23:26:12 -0400

    mod serializers to include id and foreign key fields

--- myapi/serializers.py
+++ myapi/serializers.py
@@ -5,14 +5,14 @@ from .models import Ingredient, Medication, User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = User
-		fields = ('username', 'password')
+		fields = ('id', 'username', 'password')
 
 class MedicationSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Medication
-		fields = ('name', 'user')
+		fields = ('id', 'name', 'user')
 
 class IngredientSerializer(serializers.HyperlinkedModelSerializer):
 	class Meta:
 		model = Ingredient
-		fields = ('name', 'aisle', 'shelf', 'column', 'row') 
+		fields = ('id', 'name', 'aisle', 'shelf', 'column', 'row', 'medications') 
