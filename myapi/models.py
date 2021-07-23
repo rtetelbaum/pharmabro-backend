edb46f5a3f343d31c48f12070e36e0d0df7142b0
Roman Tetelbaum <rtetelbaum@mailbox.org>
2021-07-22 22:21:43 -0400

    initial commit

---
+++ myapi/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+	username = models.CharField(max_length=60)
+	password = models.CharField(max_length=60)
+
+	def __str__(self):
+		return self.username
+
+class Ingredient(models.Model):
+	name = models.CharField(max_length=60)
+	aisle = models.CharField(max_length=60)
+	shelf = models.CharField(max_length=60)
+	column = models.CharField(max_length=60)
+	row = models.CharField(max_length=60)
+
+	def __str__(self):
+		return self.name
+
+class Medication(models.Model):
+	name = models.CharField(max_length=60)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
