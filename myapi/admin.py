2955b9f2ddeea1e4670918c81d43717fb551a6b7
Roman Tetelbaum <rtetelbaum@mailbox.org>
2021-07-22 22:38:32 -0400

    added models for medication, ingredient including manytomany

--- myapi/admin.py
+++ myapi/admin.py
@@ -5,5 +5,8 @@ from django.contrib import admin
 from .models import User
 admin.site.register(User)
 
+from .models import Medication
+admin.site.register(Medication)
+
 from .models import Ingredient
 admin.site.register(Ingredient)
--- myapi/models.py
+++ myapi/models.py
@@ -9,19 +9,20 @@ class User(models.Model):
 	def __str__(self):
 		return self.username
 
+class Medication(models.Model):
+	name = models.CharField(max_length=60)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
+
 class Ingredient(models.Model):
 	name = models.CharField(max_length=60)
 	aisle = models.CharField(max_length=60)
 	shelf = models.CharField(max_length=60)
 	column = models.CharField(max_length=60)
 	row = models.CharField(max_length=60)
-
-	def __str__(self):
-		return self.name
-
-class Medication(models.Model):
-	name = models.CharField(max_length=60)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	medications = models.ManyToManyField(Medication)
 
 	def __str__(self):
 		return self.name
